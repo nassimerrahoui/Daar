d674545336a2e0014e89f12d9109f316a0974011
3873896 <3873896@ppti-14-507-06.ufr-info-p6.jussieu.fr>
2019-10-05 17:55:30 +0200

    Patricia Tree corrigé normalement

--- src/TME3/RadixTree.java
+++ src/TME3/RadixTree.java
@@ -1,59 +1,86 @@
 package TME3;
 
+import java.awt.Point;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class RadixTree {
 	
 	HashMap<String,RadixTree> fils = new HashMap <String,RadixTree>();
+	ArrayList<Point> occurences = new ArrayList<Point>();
 	boolean isWord = true;
 	
-	public void addRadixTree(String mot) {
+	public void addRadixTree(String mot, Point pos) {
 		boolean creer_fils = true;
 		String k = "";
+		String cle = "";
 		String suffixe = "";
 		
 		for(String key : fils.keySet()) {
-			
 			char[] lettres = key.toCharArray();
 			char[] prefixes = mot.toCharArray();
 			
 			int bon_char = 0;
 			
-			if(mot.equals(key))
+			if(mot.equals(key)) {
+				isWord = true;
 				return;
+			}
 			
 			for (int i=0; i<lettres.length; i++) {
-			
+				if(i >= mot.length())
+					break;
+				
 				if(prefixes[i] == lettres[i])
 					bon_char++;
 					
 				if(prefixes[i] != lettres[i] && bon_char > 0) {
 					RadixTree t = new RadixTree();
-					t.fils.put(key.substring(bon_char),new RadixTree());
+					t.isWord = false;
+					RadixTree u = new RadixTree();
+					u.fils = fils.get(key).fils;
+					t.fils.put(key.substring(bon_char), u);
 					t.fils.put(mot.substring(bon_char),new RadixTree());
 					fils.put(key.substring(0,bon_char), t);
+					k = mot.substring(bon_char);
+					cle = key.substring(0,bon_char);
 					fils.remove(key);
 					creer_fils = false;
-					isWord = false;
-					break;
+					return;
 				}
 
 			}
+			
 			
 			if(bon_char == lettres.length) {
 				creer_fils = false;
 				k = key;
 				suffixe = mot.substring(lettres.length);
 				break;
-			}
+			} else if(bon_char == prefixes.length && mot.length() < key.length()) {
+				
+				RadixTree t = new RadixTree();
+				String suffixe2 = key.substring(mot.length());
+				
+				t.isWord = isWord;
+					
+				fils.put(key.substring(0,mot.length()), t);
+				t.fils.put(suffixe2, fils.get(key));
+				fils.remove(key);
+				
+				return;
+			} 
 		}
 		
 		if(creer_fils) {
 			fils.put(mot, new RadixTree());
-		}else {
-			fils.get(k).addRadixTree(suffixe);
+		}else if(cle != ""){
+			fils.get(cle).fils.get(k).addRadixTree(suffixe, pos);
 			
+		}else {
+			fils.get(k).addRadixTree(suffixe, pos);
 		}
+			
 	}
 	
 	public void affichage(String prefix, int profondeur) {
@@ -65,17 +92,33 @@ public class RadixTree {
 	}
 	
 	public static void main(String[] args) {
-		RadixTree t = new RadixTree();
-		t.addRadixTree("R");
-		t.addRadixTree("T");
-		t.addRadixTree("RA");
-		t.addRadixTree("RAA");
-		t.addRadixTree("RAA");
-		t.addRadixTree("RAT");
-		t.addRadixTree("RATATATATATATATATA");
+//		RadixTree t = new RadixTree();
+//		t.addRadixTree("RATATA", null);
+//		t.addRadixTree("RAT", null);
+//		t.addRadixTree("RAA", null);
+//		
+//		t.addRadixTree("RAA", null);
+//	
+//		t.addRadixTree("RA", null);
+//		t.addRadixTree("R", null);
+//		t.addRadixTree("T", null);
+//	
+//		
+//		
+//		
+//		t.affichage("", 0);
 		
-		t.affichage("", 0);
+//		RadixTree r = new RadixTree();
+//		r.addRadixTree("RATON", null);
+//		r.addRadixTree("RATIR", null);
+//		r.addRadixTree("RA", null);
+//		r.affichage("", 0);
 		
+		RadixTree o = new RadixTree();
+		o.addRadixTree("RA", null);
+		o.addRadixTree("R", null);
+		o.addRadixTree("T", null);
+		o.affichage("", 0);
 	}
 	
 }
